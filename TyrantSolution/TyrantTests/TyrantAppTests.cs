90cae56fe7b927f7e35e0e48331c0a587a8159be
Juan Huerta <juan.huerta@gmail.com>
2012-04-23 21:13:03 +0800

    Initial Commit

---
+++ TyrantSolution/TyrantTests/TyrantAppTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using NUnit.Framework;
+
+namespace TyrantTests
+{
+
+
+    [TestFixture]
+    public class TyrantAppTests
+    {
+        private Socket _connectSocket;
+
+        [Test]
+        public void Setup()
+        {
+            const int tyrantPort = 1978;
+            const string tyrantServer = "10.9.96.5";
+
+            _connectSocket = ConnectSocket(tyrantServer, tyrantPort);
+
+            Assert.That(_connectSocket.Connected, Is.True);
+        }
+
+        private static Socket ConnectSocket(string server, int port)
+        {
+            Socket s = null;
+            IPHostEntry hostEntry = null;
+
+            // Get host related information.
+            hostEntry = Dns.GetHostEntry(server);
+
+            // Loop through the AddressList to obtain the supported AddressFamily. This is to avoid
+            // an exception that occurs when the host IP Address is not compatible with the address family
+            // (typical in the IPv6 case).
+            foreach (IPAddress address in hostEntry.AddressList)
+            {
+                IPEndPoint ipe = new IPEndPoint(address, port);
+                Socket tempSocket =
+                    new Socket(ipe.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+                tempSocket.Connect(ipe);
+
+                if (tempSocket.Connected)
+                {
+                    s = tempSocket;
+                    break;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            return s;
+        }
+
+    }
+}
